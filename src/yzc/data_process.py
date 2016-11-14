828ce8fb955f3dc0a4cdd6cd13cec9a8d82e6e9e
coolyangzc <yang-zc13@mails.tsinghua.edu.cn>
2016-11-14 12:41:12 +0800

    Add validation_split in data_process

--- src/yzc/data_process.py
+++ src/yzc/data_process.py
@@ -1,37 +1,46 @@
 import numpy as np
 
 def load_data(filename, start = 0, end = -1, 
-              shuffle = True, coordinate = 'relative'):   
+              shuffle = True, coordinate = 'relative', validation_split = 0.1):   
 
     #Loading
     fd = file(filename)
     data = fd.readlines()
-    X_train = []
-    Y_train = [] 
+    X = []
+    Y = [] 
     if (end == 0):
         end = len(data)
     for line in data[start:end]:
         line_data = map(float, line.split(' '))
         a = np.array(line_data[1:2*37*65+1])
-        X_train.append(a.reshape((2, 37, 65), order='F'))
-        Y_train.append(line_data[2*37*65+1:])
-    X_train = np.array(X_train)
-    Y_train = np.array(Y_train)
+        X.append(a.reshape((2, 37, 65), order='F'))
+        Y.append(line_data[2*37*65+1:])
+    X = np.array(X)
+    Y = np.array(Y)
     
     #Processing
     if (coordinate == 'relative'):
-        for label in Y_train:
+        for label in Y:
             for i in range(3, len(label)):
                 label[i] -= label[i%3]
+        Y = np.delete(Y, np.s_[:3:], axis = 1)
     else:
         coordinate = 'absolute'
     if (shuffle):
-        r = np.random.permutation(len(Y_train))
-        X_train = X_train[r]
-        Y_train = Y_train[r]
+        r = np.random.permutation(len(Y))
+        X = X[r]
+        Y = Y[r]
+    sp = (1 - validation_split) * len(Y)
+    X_train, X_test = X[:sp], X[sp:]
+    Y_train, Y_test = Y[:sp], Y[sp:]
     
     #Output
     print ("successful loaded %d[%d:%d] datas from %s"%(len(Y_train), start, len(data) + end if end < 0 else end, filename))
     print ('shuffle = %r'%(shuffle))
     print ('coordinate = %s'%(coordinate))
-    return X_train, Y_train
+    print ('validation_split = %f'%(validation_split))
+    print 'X_train.shape =', X_train.shape
+    print 'Y_train.shape =', Y_train.shape
+    print 'X_test.shape =', X_test.shape
+    print 'Y_test.shape =', Y_test.shape
+    return (X_train, Y_train), (X_test, Y_test)
--- src/yzc/magicglove_cnn.py
+++ src/yzc/magicglove_cnn.py
@@ -1,4 +1,39 @@
 from data_process import load_data
 
+import numpy as np
+from keras.layers.core import Dense, Dropout, Activation, Flatten
+from keras.models import Sequential
+from keras.layers import Convolution2D, MaxPooling2D
+
+batch_size = 32
+nb_epoch = 10
+
+img_rows, img_cols = 37, 65
+img_channels = 2
+
 path = '../../data/'
-load_data(path + '20161111/data.txt', start = 1)
+(X_train, Y_train), (X_test, Y_test) = load_data(path + '20161111/data.txt', start = 1)
+
+model = Sequential()
+
+model.add(Convolution2D(4, 5, 5, border_mode = 'valid', input_shape = (2, 37, 65)))
+model.add(Activation('tanh'))
+
+model.add(Convolution2D(8, 3, 3, border_mode = 'valid'))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size = (2, 2)))
+
+model.add(Flatten())
+model.add(Dense(60))
+model.add(Activation('tanh'))
+
+model.compile(loss = 'mean_squared_error', optimizer = 'rmsprop', metrics = ['accuracy'])
+model.fit(X_train, Y_train, batch_size = 100, nb_epoch = 50, shuffle = True, verbose = 1, show_accuracy = True, validation_split = 0.2)
+
+result = model.predict(X[0 : 1], batch_size = 1)[0]
+print result
+print Y[0]
+print result - Y[0]
+dist = np.sqrt(np.sum(np.square(result - Y[0])))
+print dist
+
