60f5ad9acbe668704c7a3d1258b934abd2395905
coolyangzc <yang-zc13@mails.tsinghua.edu.cn>
2016-11-18 10:15:57 +0800

    Add /data in .ignore

--- src/yzc/magicglove_cnn.py
+++ src/yzc/magicglove_cnn.py
@@ -8,39 +8,56 @@ from keras.layers.convolutional import Convolution2D, MaxPooling2D
 from keras.layers.normalization import BatchNormalization
 
 batch_size = 128
-nb_epoch = 200
+nb_epoch = 128
+X_scaling = True
+Y_scaling = True
 
 img_rows, img_cols = 37, 65
 img_channels = 2
 
 path = '../../data/'
-(X_train, Y_train), (X_test, Y_test) = load_data(path + '20161111/data.txt', 
-validation_split = 0.2, start = 1, end = -1)
+(X_train, Y_train), (X_test, Y_test) = load_data(path + '20161117/data-all.txt', 
+validation_split = 0.2, start = 1, end = -2, shuffle = True)
+
+if (X_scaling):
+    X_mean = np.mean(X_train)
+    X_train -= X_mean
+    X_scale = np.std(X_train)
+    X_train /= X_scale
+else:
+    X_mean = 0.
+    X_scale = 1.
+X_test = (X_test - X_mean) / X_scale
 
 Y_train_origin = np.copy(Y_train)
 Y_test_origin = np.copy(Y_test)
 
-mean = np.mean(Y_train)
-Y_train -= mean
-scale = np.max(np.abs(Y_train)) 
-Y_train /= scale
+if (Y_scaling):
+    Y_mean = np.mean(Y_train)
+    Y_train -= Y_mean
+    Y_scale = np.max(np.abs(Y_train))
+    Y_train /= Y_scale
+else:
+    Y_mean = 0.
+    Y_scale = 1.
 
 
-Y_test -= mean
-Y_test /= scale
+Y_test = (Y_test - Y_mean) / Y_scale
+print ('(X_mean, X_scale) = (%f, %f)'%(X_mean, X_scale))
+print ('(Y_mean, Y_scale) = (%f, %f)'%(Y_mean, Y_scale))
 
 model = Sequential()
 
-model.add(Convolution2D(8, 5, 5, border_mode = 'valid', input_shape = X_train.shape[1:]))
+model.add(Convolution2D(4, 5, 5, border_mode = 'valid', input_shape = X_train.shape[1:]))
 model.add(Activation('tanh'))
 model.add(MaxPooling2D(pool_size = (2, 2)))
 
-model.add(Convolution2D(16, 3, 3, border_mode = 'valid'))
+model.add(Convolution2D(8, 3, 3, border_mode = 'valid'))
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size = (2, 2)))
 
 model.add(Flatten())
-model.add(Dense(256))
+model.add(Dense(1024))
 model.add(Activation('relu'))
 model.add(Dense(60))
 model.add(Activation('tanh'))
@@ -55,11 +72,11 @@ model.fit(X_train, Y_train,
 
 #Validation
 
-print ('mean = %f'%(mean))
-print ('scale = %f'%(scale))
+print ('(X_mean, X_scale) = (%f, %f)'%(X_mean, X_scale))
+print ('(Y_mean, Y_scale) = (%f, %f)'%(Y_mean, Y_scale))
 
 result = model.predict(X_train)
-result = result * scale + mean
+result = result * Y_scale + Y_mean
 dist = 0.
 maxdist = 0.
 for i in range(len(result)):
@@ -74,7 +91,7 @@ maxdist /= len(result)
 print ('train dist = %f(avg), %f(max)'%(dist, maxdist))
 
 result = model.predict(X_test)
-result = result * scale + mean
+result = result * Y_scale + Y_mean
 dist = 0.
 maxdist = 0.
 for i in range(len(result)):
--- src/yzc/magicglove_run.py
+++ src/yzc/magicglove_run.py
@@ -1,12 +1,15 @@
 from data_process import load_data
 from keras.models import load_model
 
-model = load_model('magicglove_model.h5')
+model = load_model('magicglove_model_3.11_9.09.h5')
 path = '../../data/'
 (X_train, Y_train), (X_test, Y_test) = load_data(path + '20161111/data.txt', 
-validation_split = 0.2, start = 1, end = -1)
+shuffle = False, validation_split = 0.2, start = 1, end = -1)
 
-scale = 84.161077
-mean = -6.688953
+X_mean = 83.619129
+X_scale = 143.878194
+Y_mean = -6.413624
+Y_scale = 93.570676
+X_train = (X_train - X_mean) / X_scale
 result = model.predict(X_train)
-result = result * scale + mean
+result = result * Y_scale + Y_mean
